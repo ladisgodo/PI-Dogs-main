69eac2d6cad57181353ebdf15c0a7c961539ae1e
Ladislao Godocik <ladislaogodocik13@gmail.com>
2022-06-21 13:36:25 -0300

    se rompio to

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import background from '../Img/background'
 
 
 export default function LandingPage() {
 
     return(
         <div>
-            <h1>Estoy en la LandingPage</h1>
-            <Link to='/home'>
-                <button>Entrar a home</button>
-            </Link>
+            <div>
+               {/*  <img src={background} alt={'background'} /> */}
+                <h1>Estoy en la LandingPage</h1>
+                <Link to='/home'>
+                    <button>Entrar a home</button>
+                </Link>
+            </div>
         </div>
     )
 }
