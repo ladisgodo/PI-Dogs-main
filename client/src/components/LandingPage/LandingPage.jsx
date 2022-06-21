5e9f13f9f278246578d102c143040a31f4a51e55
Ladislao Godocik <ladislaogodocik13@gmail.com>
2022-06-21 16:29:34 -0300

    LandingPage finished

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { Link } from 'react-router-dom';
-import background from '../Img/background'
+import background from '../Img/background.jpg'
+import styles from './LandingPage.module.css'
 
 
 export default function LandingPage() {
 
     return(
-        <div>
+        <div className={styles.container} >
+            <img src={background} alt='background' className={styles.img} ></img>
             <div>
-               {/*  <img src={background} alt={'background'} /> */}
-                <h1>Estoy en la LandingPage</h1>
+                <h1 className={styles.title} >Estoy en la LandingPage</h1>
                 <Link to='/home'>
-                    <button>Entrar a home</button>
+                    <button className={styles.btn} >Entrar a home</button>
                 </Link>
             </div>
         </div>
