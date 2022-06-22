0b8a18c96d13efebde6efc884877726ba7489b8e
Ladislao Godocik <ladislaogodocik13@gmail.com>
2022-06-22 19:46:47 -0300

    Update NavBar.jsx

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,13 +1,23 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { getDogs } from "../../redux/actions";
 import FilterName from "./Filters/FilterName/FilterName";
 import FilterWeight from "./Filters/FilterWeight/FilterWeight";
 
 export default function NavBar({setOrder}){
 
+    const dispatch = useDispatch();
+
+    function handleReset(e){
+        e.preventDefault();
+        dispatch(getDogs());
+    }
+
     return (
         <div>
             <FilterName setOrder={setOrder} />
             <FilterWeight setOrder={setOrder} />
+            <button onClick={(e) => handleReset(e) } >Reset filters</button>
         </div>
     )
 }
