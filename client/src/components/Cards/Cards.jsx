c861058c3194f376eeac8061eec506d0a202a9e0
Ladislao Godocik <ladislaogodocik13@gmail.com>
2022-06-22 15:40:03 -0300

    Update Cards.jsx

--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -5,14 +5,15 @@ import s from './Cards.module.css'
 export default function Cards({id, name, img, weightMin, weightMax, temperament}){
     return (
         <div>
-            <Link to={`/dogs/${id}`} >
                 <div className={s.container} >
                     <h1 className={s.text} >Name: {name}</h1>
                     <img src={img} alt={name} className={s.img} />
                     <h1 className={s.text} >WeightMin:{weightMin}kg weightMax:{weightMax}kg</h1>
                     <h1 className={s.text} >Temperaments: {temperament}</h1>
+                    <Link to={`/dogs/${id}`} >
+                        <h1>View details</h1>
+                    </Link>
                 </div>
-            </Link>
         </div>
     );
 }
