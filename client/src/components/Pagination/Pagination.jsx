9c47d729b8b82f6526f2919dd8a20be15e66cba6
Ladislao Godocik <ladislaogodocik13@gmail.com>
2022-06-23 21:31:00 -0300

    Update Pagination.jsx

--- client/src/components/Pagination/Pagination.jsx
+++ client/src/components/Pagination/Pagination.jsx
@@ -4,13 +4,13 @@ import s from './Pagination.module.css'
 export default function Pagination({page, setPage, max, input, setInput}){
 
     function nextPage(){
-        setInput(input + 1);
         setPage(page + 1);
+        setInput(page);
     }
 
     function prevPage(){
-        setInput(input - 1);
         setPage(page - 1);
+        setInput(page);
     }
 
     function firstPage(){
