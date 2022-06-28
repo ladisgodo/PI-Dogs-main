26defa721a0cc42a24a65b9a4cbb5e75a313fd89
Ladislao Godocik <ladislaogodocik13@gmail.com>
2022-06-28 12:39:43 -0300

    Loading lindo

--- client/src/components/Home/Loading/Loading.jsx
+++ client/src/components/Home/Loading/Loading.jsx
@@ -3,14 +3,13 @@ import loadingImage from '../../Img/Loading.jpg'
 import s from './Loading.module.css'
 
 export default function Loading({ setLoading }){
+        setTimeout(() =>{
+            setLoading(false)
+        }, 1500)
     return(
-        <div>
-            <img src={loadingImage} className={s.img} alt='loading'/>
-            {
-                setTimeout(() =>{
-                    setLoading(false)
-                }, 1500)
-            }
+        <div className={s.container}>
+            {/* <img src={loadingImage} className={s.img} alt='loading'/> */}
+            <div className={s.loading}></div>
         </div>
     )
 }
