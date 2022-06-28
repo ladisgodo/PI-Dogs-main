f3a0d7bab29f119d6727f815183dacda0c86f317
Ladislao Godocik <ladislaogodocik13@gmail.com>
2022-06-28 17:02:41 -0300

    Empezando css cards

--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -14,10 +14,10 @@ export default function Cards({dogs, page, perPage}){
                 return(
                     <div className={s.cards} key={d.id}>
                         <div className={s.content}>
-                            <h1>Name: {d.name}</h1>
+                            <h1 className={s.name}>{d.name}</h1>
                             <img src={d.image} alt={d.name} className={s.img} />
-                            <h1 className={s.text} >WeightMin:{d.weightMin}kg weightMax:{d.weightMax}kg</h1>
-                            <h1>Temperaments: {d.temperament}</h1>
+                            <h1 className={s.weight} >WeightMin:{d.weightMin}kg weightMax:{d.weightMax}kg</h1>
+                            <h1 className={s.temp}>Temperaments: {d.temperament}</h1>
                             <Link to={`/dogs/${d.id}`} >
                                 <h1>View dog</h1>
                             </Link>
