e702ef75a089b699ccbec4f8b4bef0c20235e088
Ladislao Godocik <ladislaogodocik13@gmail.com>
2022-06-30 14:00:28 -0300

    va va va

--- client/src/components/Details/Details.jsx
+++ client/src/components/Details/Details.jsx
@@ -5,6 +5,7 @@ import { clearPage, getDetails } from "../../redux/actions";
 import { Link } from "react-router-dom";
 import LoadingDetails from './LoadingDetails/LoadingDetails'
 import s from './Details.module.css'
+import backgroundHome from '../Img/backgroundHome.webp';
 
 export default function Details(){
     const { id } = useParams();
@@ -29,34 +30,37 @@ export default function Details(){
                         <LoadingDetails setLoadingDetails={setLoadingDetails}/>
                     ) : 
                     (
-                        <div className={s.details}>
-                                <div className={s.title}>
-                                    <h1 className={s.name}>{details.name}</h1>
-                                </div>
-                                <div className={s.text}>
-                                    {
-                                        details.weightMin && details.weightMax ? 
-                                        <h1 className={s.info}>Weight: {details.weightMin} kg - {details.weightMax} kg</h1>
-                                        :
-                                        <h1 className={s.info}>Weight: {details.weightMin} kg</h1>
-                                    }
-                                    {
-                                        details.heightMin && details.heightMax ? 
-                                        <h1 className={s.info}>Height: {details.heightMin} cm - {details.heightMax} cm</h1>
-                                        :
-                                        <h1 className={s.info}>Height: {details.heightMin} cm</h1>
-                                    }
-                                    {details.lifespanMax ?
-                                    <h1 className={s.info}>Life span: {details.lifespanMin} - {details.lifespanMax} years</h1> 
-                                    : <h1 className={s.info}>Life span: {details.lifespanMin}</h1>}
-                                    <h1 className={s.info}>Temperaments: {details.temperament}</h1>
-                                </div>
-                                <div className={s.span}>
-                                    <img className={s.img} src={details.image} alt={details.name}/>
-                                    <Link to='/home'>
-                                        <button className={s.btn}>Back to Home</button>
-                                    </Link>
-                                </div>
+                        <div>
+                            <img src={backgroundHome} alt='background-details' className={s.background}/>
+                            <div className={s.details}>
+                                    <div className={s.title}>
+                                        <h1 className={s.name}>{details.name}</h1>
+                                    </div>
+                                    <div className={s.text}>
+                                        {
+                                            details.weightMin && details.weightMax ? 
+                                            <h1 className={s.info}>Weight: {details.weightMin} kg - {details.weightMax} kg</h1>
+                                            :
+                                            <h1 className={s.info}>Weight: {details.weightMin} kg</h1>
+                                        }
+                                        {
+                                            details.heightMin && details.heightMax ? 
+                                            <h1 className={s.info}>Height: {details.heightMin} cm - {details.heightMax} cm</h1>
+                                            :
+                                            <h1 className={s.info}>Height: {details.heightMin} cm</h1>
+                                        }
+                                        {details.lifespanMax ?
+                                        <h1 className={s.info}>Life span: {details.lifespanMin} - {details.lifespanMax} years</h1> 
+                                        : <h1 className={s.info}>Life span: {details.lifespanMin}</h1>}
+                                        <h1 className={s.info}>Temperaments: {details.temperament}</h1>
+                                    </div>
+                                    <div className={s.span}>
+                                        <img className={s.img} src={details.image} alt={details.name}/>
+                                        <Link to='/home'>
+                                            <button className={s.btn}>Back to Home</button>
+                                        </Link>
+                                    </div>
+                            </div>
                         </div>
                     )
                 }
